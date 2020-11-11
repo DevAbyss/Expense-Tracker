9a3e4c689c6523f29ce7e35ef8885f50b31cfbb3
LEEHEEJOON <marco920322@gmail.com>
2020-11-12 00:29:28 +0900

    Modify components - Transaction, TransactionList, Balance, IncomeExpenses
    Modify context
    - GlobalStates

--- src/context/GlobalStates.js
+++ src/context/GlobalStates.js
@@ -4,10 +4,10 @@ import AppReducer from './AppReducer';
 // Initial state
 const initialState = {
   transactions: [
-    { id: 1, text: 'Flower', amout: -20 },
-    { id: 2, text: 'Salary', amout: 300 },
-    { id: 3, text: 'Book', amout: -10 },
-    { id: 4, text: 'Camera', amout: 150 },
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 },
   ],
 };
 
